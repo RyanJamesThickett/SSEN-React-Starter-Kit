58697a32fb5cd67937debe75998196585b719b21
Charlie Carolan <charliecarolan1999@gmail.com>
2025-10-15 21:50:50 +0100

    feat: working api example

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const headers = {
+	"Authorization": "Bearer <TOKEN>"
+}
+
 // https://vite.dev/config/
 export default defineConfig({
+	server: {
+		proxy: {
+			"/api": {
+				target: 'https://nerda-prod-apis-v2.azurewebsites.net/api/ApiNerdaStatic?substation=74f42299-9f8e-4cb4-922c-0e3273bff4c7',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ''),
+				headers,
+			}
+		}
+	},
   plugins: [
     react({
       babel: {
